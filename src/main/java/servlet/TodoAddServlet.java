486186c13f02d27410714920e2a2f29f485cacad
LeeNaYoung <iirony@naver.com>
2018-06-03 00:11:49 +0900

    테이블에 저장하고 메인화면으로 리다이렉트

---
+++ src/main/java/servlet/TodoAddServlet.java
@@ -0,0 +1,52 @@
+package servlet;
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import Todo.dao.TodoDao;
+import Todo.dto.TodoDto;
+
+/**
+ * Servlet implementation class TodoAddServlet
+ */
+@WebServlet("/add")
+public class TodoAddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public TodoAddServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setCharacterEncoding("utf-8");
+		
+		// 값가져오기
+	    String what = request.getParameter("what");
+	    String who = request.getParameter("who");
+	    int s = Integer.parseInt(request.getParameter("sequence"));
+	    
+		//TodoDao를 이용해서 테이블에 저장		
+		TodoDto dto=new TodoDto();
+		dto.setTitle(what);
+		dto.setName(who);
+		dto.setSequence(s);
+		TodoDao dao=new TodoDao();
+		dao.addTodo(dto);
+		
+		//메인화면으로 리다이렉트
+		response.sendRedirect("main.jsp");
+	}
+
+}
